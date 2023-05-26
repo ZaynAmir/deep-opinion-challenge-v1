d3b8f3c3f41c044893c73286a64b26dc59ff1462
ZaynAmir <zaynsheikhofficial@gmail.com>
2023-05-26 16:52:32 +0500

    made models schema for Tag, Sheet, TraningData in models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    aspect = models.CharField(max_length=100)
+    sentiment = models.CharField(max_length=3, choices=(
+        ('POS', 'Positive'),
+        ('NEG', 'Negative'),
+        ('NEU', 'Neutral'),
+    ))
+
+    def __str__(self) -> str:
+        return f"aspect: {self.aspect} - sentiment: {self.sentiment}"
+
+
+class SheetModel(models.Model):
+   name = models.CharField(max_length=255)
+   row_count = models.PositiveBigIntegerField()
+
+   def __str__(self) -> str:
+        return f"{self.name} sheets has {self.row_count} rows"
+
+class TrainingData(models.Model):
+    text = models.TextField(null=True)
+    tags = models.ManyToManyField(Tag)
+    sheet = models.ForeignKey(SheetModel, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.text
+
